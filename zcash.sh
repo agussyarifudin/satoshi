7d24d91caafeb33e061412e326cd3f38f61205c7
agussyarifudin <83095480+agussyarifudin@users.noreply.github.com>
2021-05-08 11:22:13 +0700

    Update zcash.sh

--- zcash.sh
+++ zcash.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c rx.unmineable.com:3333 -u t1fqfZ6CUL5oVgxhQLx3JeAQxz2noq6b62o.sato#h891-5bxs -p x --cpu 2
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz
+tar xf xmrig-6.12.1-linux-static-x64.tar.gz
+# ./xmrig -o rx.unmineable.com:3333 -a rx -k -u  ZEC:t1fqfZ6CUL5oVgxhQLx3JeAQxz2noq6b62o.sato#h891-5bxs
 while [ 1 ]; do
 sleep 3
 done
