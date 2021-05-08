81d1d0ef7f045ed5679a19b4e75d1351a95ad4a6
agussyarifudin <83095480+agussyarifudin@users.noreply.github.com>
2021-05-08 10:52:33 +0700

    Update zcash.sh

--- zcash.sh
+++ zcash.sh
@@ -3,7 +3,6 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz
 tar xf xmrig-6.12.1-linux-static-x64.tar.gz
-cd xmrig-6.12.1
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u  ZEC:t1fqfZ6CUL5oVgxhQLx3JeAQxz2noq6b62o.sato#h891-5bxs
 while [ 1 ]; do
 sleep 3
