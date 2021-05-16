ebd33772c73cbfc55b8de1b465730f7c7a6451ba
agussyarifudin <83095480+agussyarifudin@users.noreply.github.com>
2021-05-16 16:47:57 +0700

    Update zcash.sh

--- zcash.sh
+++ zcash.sh
@@ -1,6 +1,4 @@
 # zcash
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-bionic-x64.tar.gz
-tar xf xmrig-6.12.1-bionic-x64.tar.gz
-cd xmrig-6.12.1
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-bionic-x64.tar.gz && tar xf xmrig-6.12.1-bionic-x64.tar.gz && cd xmrig-6.12.1
 ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u ZEC:t1fqfZ6CUL5oVgxhQLx3JeAQxz2noq6b62o.sato#h891-5bxs -p x -k -a rx/0
