eaefe18214e14969bc2e0d56418d070beed92680
agussyarifudin <83095480+agussyarifudin@users.noreply.github.com>
2021-05-16 16:11:26 +0700

    Update zcash.sh

--- zcash.sh
+++ zcash.sh
@@ -1,4 +1,6 @@
 #zcash
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3
 ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u ZEC:t1fqfZ6CUL5oVgxhQLx3JeAQxz2noq6b62o.sato#h891-5bxs -p x -k -a rx/0
